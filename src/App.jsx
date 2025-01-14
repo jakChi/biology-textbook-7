a5ca267bb40977f9ceaa614343093a3c3a4ccd72
jakChi <kobachincharauli8@gmail.com>
2025-01-14 13:40:44 +0400

    embaded page from scribd

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="w-full h-screen border border-red-500">
+      <iframe
+        title="ბიოლოგია-7-მოსწავლის-წიგნი"
+        src="https://www.scribd.com/embeds/792798177/content?start_page=1&view_mode=scroll&access_key=key-hCCBspyijgt1y2ADfxw2"
+        data-auto-height="true"
+        data-aspect-ratio="0.7088906372934697"
+        width="100%"
+        height="600"
+        className="h-full"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
